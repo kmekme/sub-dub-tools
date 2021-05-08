b9ce325dd43ffbc21440f5807f1b21cb9115e640
kmekme <kmefansub@gmail.com>
2021-05-09 02:08:44 +0300

    Error handling

--- dub.characters.py
+++ dub.characters.py
@@ -10,24 +10,44 @@ parser.add_argument('-i', '--input', metavar='FILENAME',
 args = parser.parse_args()
 
 #convert the rtf file to text
-tokens = args.input.rsplit('.', 1)
-input_text = tokens[0] + '.txt'
-file = os.getcwd() + '/' + input_text
-os.popen('/usr/bin/unoconv -d document -f text "{}"'.format(args.input))
-while True:
-	sleep(0.1)
-	if os.path.isfile(file): break
+try:
+	tokens = args.input.rsplit('.', 1)
+	input_text = tokens[0] + '.txt'
+	file = os.getcwd() + '/' + input_text
+	os.popen('/usr/bin/unoconv -d document -f text "{}"'.format(args.input))
+	while True:
+		sleep(0.1)
+		if os.path.isfile(file): break
+except FileNotFoundError:
+	sys.exit('Unable to open {}. Does the file exist?'.format(args.input))
+except PermissionError:
+	sys.exit('Unable to open {}. Check your file permissions.'.format(args.input))
+except:
+	sys.exit('Something broke when converting the rtf file to text.')
 
 #list the characters
-with open(file, 'r') as script:
-	content = script.read()
-	characters = re.findall(r'\[(.*?)\]', content)
-	printed = list()
-	for character in characters:
-		if not character in printed:
-			print("[{}]".format(character))
-			printed.append(character)
-
+try:
+	with open(file, 'r') as script:
+		content = script.read()
+		characters = re.findall(r'\[(.*?)\]', content)
+		printed = list()
+		for character in characters:
+			if not character in printed:
+				print("[{}]".format(character))
+				printed.append(character)
+except FileNotFoundError:
+        sys.exit('Unable to open {}. Does the file exist?'.format(input_text))
+except PermissionError:
+        sys.exit('Unable to open {}. Check your file permissions.'.format(input_text))
+except:
+	sys.exit('Something broke when looking for terms.')
 
 #delete the text file
-os.remove(file)
+try:
+	os.remove(file)
+except FileNotFoundError:
+        sys.exit('Unable to open {}. Does the file exist?'.format(file))
+except PermissionError:
+        sys.exit('Unable to open {}. Check your file permissions.'.format(file))
+except:
+        sys.exit('Something broke when deleting {}.'.format(file))
